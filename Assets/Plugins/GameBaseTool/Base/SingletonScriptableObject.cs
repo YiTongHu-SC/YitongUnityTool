6f584cff6bb30119ce491e032156922e0ab5f7b7
huyitong <cquhbb@163.com>
2025-09-15 14:09:23 +0800

    add SingletonScriptableObject

---
+++ Assets/Plugins/GameBaseTool/Base/SingletonScriptableObject.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace Plugins.GameBaseTool.Base
+{
+    public abstract class SingletonScriptableObject<T> : ScriptableObject where T : SingletonScriptableObject<T>
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = Resources.Load<T>(typeof(T).Name);
+                    if (_instance == null)
+                    {
+                        throw new System.Exception("Can not find any singleton scriptable object in the resource.");
+                    }
+                    // var assets = Resources.LoadAll<T>($"");
+                    // if (assets == null || assets.Length == 0)
+                    // {
+                    //     throw new System.Exception("Can not find any singleton scriptable object in the resource.");
+                    // }
+                    // else if (assets.Length > 1)
+                    // {
+                    //     Debug.LogWarning("Multiple singleton scriptable objects found in resource. ");
+                    // }
+                    //
+                    // _instance = assets[0];
+                }
+                return _instance;
+            }
+        }
+        public abstract void Init();
+    }
+}
